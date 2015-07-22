679385d09e6d6b1dc8fce8a4f976647fcec739c2
James <dontwantto@gmail.com>
2015-07-22 10:44:55 -0400

    This is with the new fork comments

--- fork.c
+++ fork.c
@@ -1,12 +1,25 @@
 #include <unistd.h>
 #include <stdio.h>
 
+void child_function(void) {
+	sleep(3);
+}
+
 int main(int argc, char *argv[])
 {
 	pid_t child = fork();
+	int datum = 3;
 	if(child == -1) {
 		perror("Could not fork");
-	} else if (child) {
+	} else if (child) { //Parent
 		printf("New process is %d\n", child);
+		//printf("datum = %d\n", datum);
+	} else { //Child
+		child_function();
+		printf("I am the child\n");
+		datum = 12;
+		//printf("datum = %d\n", datum);
 	}
+	printf("%d datum = %d (%p)\n", child, datum, &datum);
+	return 0;
  }
